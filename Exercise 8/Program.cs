f388eb260800f11f6ae11866d6e0382131279e16
KoreanFubuki <145092552+KoreanFubuki@users.noreply.github.com>
2023-09-19 16:50:02 +0400

    Add project files.

---
+++ Exercise 8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            run();
+            Console.Read();
+        }
+        public static void run ()
+        {
+            int i = -10;
+            while (true)
+            {
+                if (i % 3 == 0)
+                {
+                    i++;
+                    continue;
+                }
+                if (i == 10)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
+
+
+    
